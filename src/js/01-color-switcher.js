7a8929649aa0a20af96ad309a1eb31ddd61d659d
Oleksandr Arseniuk <korol1511@gmail.com>
2021-11-15 18:36:49 +0200

    Сделано первое ДЗ

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+let btnStart = document.querySelector('button[data-start]');
+let btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+let changeColor = document.body.style.backgroundColor;
+btnStart.classList.add('js-style');
+console.log(btnStart);
+console.log(btnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(
+        document.body.style.backgroundColor = getRandomHexColor(), 1000)
+    btnStart.setAttribute("disabled", "disabled");
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStart.removeAttribute("disabled");
+});
+
+
+
 
