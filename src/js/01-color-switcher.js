cbcf99bbd618cda2a9a4c10fb925f5c8d1e513e4
Oleksandr Arseniuk <korol1511@gmail.com>
2021-11-16 13:48:26 +0200

    Внесены правки в color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,19 +2,19 @@ let btnStart = document.querySelector('button[data-start]');
 let btnStop = document.querySelector('button[data-stop]');
 let timerId = null;
 let changeColor = document.body.style.backgroundColor;
-btnStart.classList.add('js-style');
-console.log(btnStart);
-console.log(btnStop);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-btnStart.addEventListener('click', () => {
-    timerId = setInterval(
-        document.body.style.backgroundColor = getRandomHexColor(), 1000)
-    btnStart.setAttribute("disabled", "disabled");
-});
+btnStart.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute("disabled", "disabled");
+}
 
 btnStop.addEventListener('click', () => {
     clearInterval(timerId);
