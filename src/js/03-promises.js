ceabf95f3b176c650001ce21bb798d599b169507
Oleksandr Arseniuk <korol1511@gmail.com>
2021-11-25 19:42:33 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -23,26 +23,26 @@ function onBtnClick(event) {
 
   for (let i = 0; i < amountEl; i += 1) {
     const position = i + 1;
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    createPromise(position, delayEl)
+      .then(({ position, delayEl }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delayEl}ms`);
       })
-      .catch(({ position, delay }) => {
-        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      .catch(({ position, delayEl }) => {
+        Notify.failure(`Rejected promise ${position} in ${delayEl}ms`);
       });
     delayEl += stepEl;
   }
 }
 
-function createPromise(position, delay) {
+function createPromise(position, delayEl) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay });
+        resolve({ position, delayEl });
       } else {
-        reject({ position, delay });
+        reject({ position, delayEl });
       }
-    }, delay);
+    }, delayEl);
   });
 }
